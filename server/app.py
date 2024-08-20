9cd0243446d81bb59892b7701992734b6f1c9e30
0097eo <emmanuelokello294@gmail.com>
2024-08-20 10:58:38 +0300

    add authentication

--- server/app.py
+++ server/app.py
@@ -1,5 +1,34 @@
-from config import app, db
+from config import app, db, api
 from models import User, Admin, Student, Cohort, Project
+from flask import request
+from flask_restful import Resource
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
+from sqlalchemy import or_
+from datetime import timedelta, datetime
 
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        if not username or not password:
+            return {'error': 'Missing username or password'}, 400
+
+        user = User.query.filter(or_(User.username == username, User.email == username)).first()
+
+        if not user or not user.check_password(password):
+            return {'error': 'Invalid username or password'}, 401
+
+        access_token = create_access_token(identity=user.id, expires_delta=timedelta(days=10))
+        refresh_token = create_refresh_token(identity=user.id)
+
+        return {
+            'access_token': access_token,
+           'refresh_token': refresh_token
+        }, 200
+
+api.add_resource(Login, '/login')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5555, debug=True)
