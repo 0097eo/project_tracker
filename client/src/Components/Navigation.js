e0ae5da83de00190d807fd8a02eea316d6826500
0097eo <emmanuelokello294@gmail.com>
2024-08-26 09:24:36 +0300

    create navigation component

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../Utils/AuthContext';
 
 const Navigation = () => {
-  return (
-    <div>Navigation</div>
-  )
-}
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
-export default Navigation
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        {!user && (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        )}
+        {user && user.is_admin && (
+          <li><Link to="/admin-dashboard">Admin Dashboard</Link></li>
+        )}
+        {user && !user.is_admin && (
+          <li><Link to="/student-dashboard">Student Dashboard</Link></li>
+        )}
+        {user && (
+          <li><button onClick={handleLogout}>Logout</button></li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
